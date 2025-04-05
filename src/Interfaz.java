b0316d8a3c8d8adc506492b8efcb8d8659da66ba
PedroMG2023 <devmgcoding03@gmail.com>
2025-04-04 19:27:01 -0600

    Detalles del ejercicio de cajero automatico

---
+++ src/Interfaz.java
@@ -0,0 +1,21 @@
+/*
+Programa que emula la interfaz de un cajero automático. El programa presentará las siguientes opciones al usuario de forma indefinida hasta que éste introduzca un número diferente a los indicados:
+
+Elija una de las siguientes opciones:
+    Escriba 1 para consultar su saldo
+    Escriba 2 para ingresar dinero
+    Escriba 3 para sacar dinero
+    Escriba 4 para consultar sus últimos movimientos
+
+Una vez escrita la opción, pulse la tecla Enter
+
+Una vez el usuario ha elegido la opción, el programa le mostrará por pantalla una frase explicando la opción elegida
+*/
+
+public class Interfaz {
+
+    public static void main(String[] args) {
+
+
+    }
+}
